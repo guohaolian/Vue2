e4edc49a974f2802896d581f5124cf583adab759
guohaolian <1040071899@qq.com>
2025-11-20 21:17:35 +0800

    03

---
+++ vue_test/12_src_全局事件总线/components/School.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="school">
+		<h2>学校名称：{{ name }}</h2>
+		<h2>学校地址：{{ address }}</h2>
+		<h2>{{ studentName }}</h2>
+		<h2>{{ sex }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'School',
+	data() {
+		return {
+			name: '尚硅谷',
+			address: '北京',
+			studentName: '',
+			sex: ''
+		}
+	},
+	mounted() {
+		// console.log('School',this)
+		this.$bus.$on('guohaolian', (...data) => {
+			console.log('我是School组件，收到了数据', data[0]);
+			//this.studentName = data[0];
+			//this.sex = data[1];
+			const [studentName, sex] = data;
+			this.studentName = studentName;
+			this.sex = sex;
+
+		})
+	},
+	beforeDestroy() {
+		this.$bus.$off('guohaolian')
+	},
+}
+</script>
+
+<style scoped>
+.school {
+	background-color: skyblue;
+	padding: 5px;
+}
+</style>
