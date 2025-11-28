16accd865e7e8355bfa75b389bc9cfd680038cb0
guohaolian <1040071899@qq.com>
2025-11-28 19:44:32 +0800

    09

--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,27 +1,12 @@
 <template>
-  <!-- 可以没有根标签，因为Vue会自动添加一个根标签 -->
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+	<Demo/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import Demo from './components/Demo'
+	export default {
+		name: 'App',
+		components:{Demo},
+	}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
