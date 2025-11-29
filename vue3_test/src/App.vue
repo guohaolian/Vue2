67fb0f4ab36fdae1c3b4a4f754aaea44b42d64a4
guohaolian <1040071899@qq.com>
2025-11-29 13:19:53 +0800

    10

--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,18 +1,27 @@
 <template>
-	<button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
-	<Demo v-if="isShowDemo"/>
+	<div class="app">
+		<h3>我是App组件（祖），{{ name }}--{{ price }}</h3>
+		<Child />
+	</div>
 </template>
 
 <script>
-	import {ref} from 'vue'
-	import Demo from './components/Demo'
-	export default {
-		name: 'App',
-		components:{Demo},
-		setup() {
-			let isShowDemo = ref(true)
-			return {isShowDemo}
-		}
+import { reactive, toRefs, provide } from 'vue'
+import Child from './components/Child.vue'
+export default {
+	name: 'App',
+	components: { Child },
+	setup() {
+		let car = reactive({ name: '奔驰', price: '40W' })
+		provide('Car', car) //给自己的后代组件传递数据
+		return { ...toRefs(car) }
 	}
+}
 </script>
 
+<style>
+.app {
+	background-color: gray;
+	padding: 10px;
+}
+</style>
